13fd75d607e2680d570c344c0b0023091ea3568f
davidZahedi <davidza001@gmail.com>
2018-11-20 15:19:41 +0100

    doc string

--- function.py
+++ function.py
@@ -1,8 +1,30 @@
 from scipy import stats
 from math import log
 
-def dist_log_loss(true, prob, lab_ord=[]):
-    """Description to come"""
+def dist_log_loss(y_true, ypred, labels=[]):
+    """Log loss, aka logistic loss or cross-entropy loss.
+    
+    For a single sample with true label yt in {0,1} and
+    estimated probability yp that yt = 1, the log loss is
+
+        -log P(yt|yp) = -(yt log(yp) + (1 - yt) log(1 - yp))
+    
+    Parameters
+    ----------
+    y_true : array-like or label indicator matrix
+        Ground truth (correct) labels for n_samples samples.
+
+    y_pred : array-like of float, shape = (n_samples, n_classes) 
+        Predicted probabilities      
+   
+
+    labels : array-like, optional (default=None)
+        If not provided, labels will be inferred from y_true. 
+
+    Returns
+    -------
+    loss : float
+    """
     
     losses = []
     if len(lab_ord):
